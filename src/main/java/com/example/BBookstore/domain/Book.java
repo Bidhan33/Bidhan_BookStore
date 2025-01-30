6b099a778e03ad515719b1553d5b95230a80ad96
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-01-30 08:52:10 +0200

    first commit

---
+++ src/main/java/com/example/BBookstore/domain/Book.java
@@ -0,0 +1,59 @@
+package com.example.BBookstore.domain;
+
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private double price;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
