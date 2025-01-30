2e6bf5c81c3e91c1acd057e1685d6b4b5cf16677
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-01-30 10:27:33 +0200

    Solution of Task1

--- src/main/java/com/example/BBookstore/web/BookController.java
+++ src/main/java/com/example/BBookstore/web/BookController.java
@@ -1,13 +1,56 @@
 package com.example.BBookstore.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.BBookstore.domain.Book;
+import com.example.BBookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+    
     @GetMapping("/index")
     public String index() {
-        return "index"; 
+        return "index";
+    }
+    
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
+    }
+    
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        Book book = bookRepository.findById(bookId)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + bookId));
+        model.addAttribute("book", book);
+        return "editbook";
+    }
+    
+    @PostMapping("/savebook")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/booklist";
     }
 }
